5c1d9ae88bc7457344d9f685f4798e64aade1ac6
hyun0404woo <hyun0404woo@gmail.com>
2022-05-16 15:05:02 +0900

    Refactor: ExportToExcel refactoring
    - Change variable with '_' to camel case
    - Extract method
      - setJFileChooser()
    - Set Constructor
    - Create ExportManager Interface for OCP

---
+++ src/main/java/utils/ExportManager.java
@@ -0,0 +1,10 @@
+package utils;
+
+import javax.swing.*;
+
+public interface ExportManager {
+    
+    public JFileChooser setJFileChooser();
+
+//    public void exportResults();
+}
--- src/main/java/utils/ExportToExcel.java
+++ src/main/java/utils/ExportToExcel.java
@@ -15,23 +15,46 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-public class ExportToExcel {
-    private static final Logger log = LoggerFactory.getLogger(ExportToExcel.class);
+import static javax.swing.JFileChooser.*;
+
+
+public class ExportToExcel implements ExportManager{
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ExportToExcel.class);
+
+    private final String extension = ".xls";
+
+    private FileNameExtensionFilter extensionFilter;
+    private File file;
+    private JFileChooser jFileChooser;
+    
+
+    public ExportToExcel() {
+        this.extensionFilter = new FileNameExtensionFilter("*.xls", "*.xls", "*.XLS", "*.*");
+        this.jFileChooser = setJFileChooser();
+        this.file = new File(jFileChooser.getSelectedFile() + extension);
+    }
+    
+
+    @Override
+    public JFileChooser setJFileChooser() {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setFileFilter(extensionFilter);
+        jFileChooser.setCurrentDirectory(new File
+                (System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
+        return jFileChooser;
+    }
+
 
     public void exportResultsToExcel() {
         try {
-            //Save file to
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("*.xls", "*.xls", "*.XLS", "*.*");
-            JFileChooser save_to = new JFileChooser();
-            save_to.setFileFilter(filter);
-            save_to.setCurrentDirectory(new File
-                    (System.getProperty("user.home") + System.getProperty("file.separator")+ "Desktop"));
-            int ret = save_to.showDialog(null,"Save");
-            if (ret == JFileChooser.APPROVE_OPTION) {
-                File file = new File(save_to.getSelectedFile() + ".xls");
+//            JFileChooser jFileChooser = setJFileChooser();
 
-                WritableWorkbook new_excel = Workbook.createWorkbook(file);
-                WritableSheet page = new_excel.createSheet("001", 0);
+            if (jFileChooser.showDialog(null,"Save") == APPROVE_OPTION) {
+//                File file = new File(jFileChooser.getSelectedFile() + ".xls");
+
+                WritableWorkbook newExcel = Workbook.createWorkbook(file);
+                WritableSheet page = newExcel.createSheet("001", 0);
                 page.getSettings().setShowGridLines(true);
                 page.setColumnView(0, 10);
                 page.setColumnView(1, 16);
@@ -41,44 +64,44 @@ public class ExportToExcel {
                 page.setRowView(0, 600);
 
                 //no bold
-                WritableFont wf = new WritableFont(WritableFont.ARIAL, 11,
+                WritableFont writableFontNoBold = new WritableFont(WritableFont.ARIAL, 11,
                         WritableFont.NO_BOLD, false, UnderlineStyle.NO_UNDERLINE,
                         jxl.format.Colour.BLACK);
 
                 //bold
-                WritableFont wf_bold = new WritableFont(WritableFont.ARIAL, 11,
+                WritableFont writableFontBold = new WritableFont(WritableFont.ARIAL, 11,
                         WritableFont.BOLD, false, UnderlineStyle.NO_UNDERLINE,
                         jxl.format.Colour.BLACK);
 
                 //Hyperlinks
-                WritableFont wf_link = new WritableFont(WritableFont.ARIAL, 11, WritableFont.NO_BOLD);
-                wf_link.setColour(Colour.DARK_GREEN);
-                WritableCellFormat wcf_link = new WritableCellFormat(wf_link);
-                wcf_link.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcf_link.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_link.setWrap(true);
+                WritableFont hyperlinkFont = new WritableFont(WritableFont.ARIAL, 11, WritableFont.NO_BOLD);
+                hyperlinkFont.setColour(Colour.DARK_GREEN);
+                WritableCellFormat hyperlinkCellFormat = new WritableCellFormat(hyperlinkFont);
+                hyperlinkCellFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                hyperlinkCellFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                hyperlinkCellFormat.setWrap(true);
 
                 //WritableCellFormat wcf_no_border = new WritableCellFormat(wf);
 
-                WritableCellFormat wcf = new WritableCellFormat(wf);
+                WritableCellFormat wcf = new WritableCellFormat(writableFontNoBold);
                 wcf.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
                 wcf.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
                 wcf.setWrap(true);
 
-                WritableCellFormat wcf_centre_no_bold = new WritableCellFormat(wf);
-                wcf_centre_no_bold.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcf_centre_no_bold.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_centre_no_bold.setAlignment(Alignment.CENTRE);
+                WritableCellFormat wcfCentreNoBold = new WritableCellFormat(writableFontNoBold);
+                wcfCentreNoBold.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                wcfCentreNoBold.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                wcfCentreNoBold.setAlignment(Alignment.CENTRE);
 
                 //no bold
-                WritableCellFormat wcf_centre = new WritableCellFormat(wf);
+                WritableCellFormat wcf_centre = new WritableCellFormat(writableFontNoBold);
                 wcf_centre.setAlignment(jxl.format.Alignment.CENTRE);
                 wcf_centre.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
                 wcf_centre.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
                 wcf_centre.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
 
                 //HEADERS: color, bold
-                WritableCellFormat wcf_centre_bold = new WritableCellFormat(wf_bold);
+                WritableCellFormat wcf_centre_bold = new WritableCellFormat(writableFontBold);
                 wcf_centre_bold.setAlignment(jxl.format.Alignment.CENTRE);
                 wcf_centre_bold.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
                 wcf_centre_bold.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
@@ -86,12 +109,12 @@ public class ExportToExcel {
 
                 //DATE: no bold
                 DateFormat dateFormat = new DateFormat("dd-MM-yyyy HH:mm") ;
-                WritableCellFormat wcf_date = new WritableCellFormat(dateFormat);
-                wcf_date.setAlignment(jxl.format.Alignment.CENTRE);
-                wcf_date.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_date.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcf_date.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_date.setAlignment(Alignment.CENTRE);
+                WritableCellFormat wcfDate = new WritableCellFormat(dateFormat);
+                wcfDate.setAlignment(jxl.format.Alignment.CENTRE);
+                wcfDate.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                wcfDate.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                wcfDate.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                wcfDate.setAlignment(Alignment.CENTRE);
 
                 String[] headers = {"Number", "Source", "Title", "Date", "Link"};
                 for (int s = 0; s < headers.length; s++) {
@@ -100,12 +123,12 @@ public class ExportToExcel {
                 }
 
                 for (int z = 0; z < Gui.model.getRowCount(); z++) {
-                    jxl.write.Number y1 = new jxl.write.Number(0, z + 1, Integer.parseInt(Gui.model.getValueAt(z, 0).toString()), wcf_centre_no_bold); //num
-                    Label y2 = new Label(1, z + 1, Gui.model.getValueAt(z, 1).toString(), wcf_centre_no_bold); //Source
+                    jxl.write.Number y1 = new jxl.write.Number(0, z + 1, Integer.parseInt(Gui.model.getValueAt(z, 0).toString()), wcfCentreNoBold); //num
+                    Label y2 = new Label(1, z + 1, Gui.model.getValueAt(z, 1).toString(), wcfCentreNoBold); //Source
                     Label y3 = new Label(2, z + 1, Gui.model.getValueAt(z, 2).toString(), wcf); //Title
-                    Label y4 = new Label(3, z + 1, Gui.model.getValueAt(z, 3).toString(), wcf_date); //Date
+                    Label y4 = new Label(3, z + 1, Gui.model.getValueAt(z, 3).toString(), wcfDate); //Date
                     //Link
-                    Label y5 = new Label(4, z + 1, Gui.model.getValueAt(z, 4).toString(), wcf_link);
+                    Label y5 = new Label(4, z + 1, Gui.model.getValueAt(z, 4).toString(), hyperlinkCellFormat);
                     WritableHyperlink hl = new WritableHyperlink(4, z + 1, new URL(Gui.model.getValueAt(z, 4).toString()));
                     page.addHyperlink(hl);
                     page.addCell(y1);
@@ -115,13 +138,15 @@ public class ExportToExcel {
                     page.addCell(y5);
                     page.setRowView(z + 1, 600);
                 }
-                new_excel.write();
-                new_excel.close();
+                newExcel.write();
+                newExcel.close();
+                
                 Common.console("status: export is done");
+                
             } else Common.console("status: export canceled");
         } catch (WriteException | IOException e) {
             Common.console("status: export exception");
-            log.warn("export exception");
+            LOG.warn("export exception");
         }
     }
 }
