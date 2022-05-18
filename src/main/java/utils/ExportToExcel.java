a8f774f4cfed3e8d8e28ba1d6991b03eba90052f
hyun0404woo <hyun0404woo@gmail.com>
2022-05-18 10:43:44 +0900

    Refactor: Create ExportManager class
    - ExportToExcel extends ExportManager
    
    - ExportManager has private fields
    (headers, extension, extensionFilter jFileChooser)
    and getter, setter.
    The reason for inheritance is that it is a required field even if it is not an xls file.
    
    - Extract initPage method

--- src/main/java/utils/ExportToExcel.java
+++ src/main/java/utils/ExportToExcel.java
@@ -19,48 +19,22 @@ import java.net.URL;
 import static javax.swing.JFileChooser.*;
 
 
-public class ExportToExcel implements ExportManager{
-    
-    private static final Logger LOG = LoggerFactory.getLogger(ExportToExcel.class);
+public class ExportToExcel extends ExportManager {
 
-    private final String extension = ".xls";
-
-    private String[] headers = {"Number", "Source", "Title", "Date", "Link"};
-    private FileNameExtensionFilter extensionFilter;
     private File baseExcelFile;
-    private JFileChooser jFileChooser;
     
-
     public ExportToExcel() {
-        this.extensionFilter = new FileNameExtensionFilter("*.xls", "*.xls", "*.XLS", "*.*");
-        this.jFileChooser = setJFileChooser();
-        this.baseExcelFile = new File(jFileChooser.getSelectedFile() + extension);
-    }
-    
-
-    @Override
-    public JFileChooser setJFileChooser() {
-        JFileChooser jFileChooser = new JFileChooser();
-        jFileChooser.setFileFilter(extensionFilter);
-        jFileChooser.setCurrentDirectory(new File
-                (System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
-        return jFileChooser;
-    }
-
-
-    private void initPage(WritableSheet page) throws WriteException, RowsExceededException {
-        page.getSettings().setShowGridLines(true);
-        page.setColumnView(0, 10);
-        page.setColumnView(1, 16);
-        page.setColumnView(2, 100);
-        page.setColumnView(3, 30);
-        page.setColumnView(4, 120);
-        page.setRowView(0, 600);
+        setExtension(".xls");
+        setExtensionFilter(new FileNameExtensionFilter("*.xls", "*.xls", "*.XLS", "*.*"));
+        setjFileChooser();
     }
     
     public void exportResultsToExcel() {
+
+        baseExcelFile = new File(getjFileChooser().getSelectedFile() + getExtension());
+        
         try {
-            if (jFileChooser.showDialog(null,"Save") == APPROVE_OPTION) {
+            if (getjFileChooser().showDialog(null,"Save") == APPROVE_OPTION) {
 
                 WritableWorkbook newExcel = Workbook.createWorkbook(baseExcelFile);
                 WritableSheet page = newExcel.createSheet("001", 0);
@@ -131,14 +105,14 @@ public class ExportToExcel implements ExportManager{
 
                 for (int z = 0; z < Gui.model.getRowCount(); z++) {
                     jxl.write.Number jxlNumber = new jxl.write.Number(0, z + 1, Integer.parseInt(Gui.model.getValueAt(z, 0).toString()), cellNumberFormat); //num
-                    
+
                     Label source = new Label(1, z + 1, Gui.model.getValueAt(z, 1).toString(), cellSourceFormat); //Source
                     Label title = new Label(2, z + 1, Gui.model.getValueAt(z, 2).toString(), cellTitleFormat); //Title
                     Label date = new Label(3, z + 1, Gui.model.getValueAt(z, 3).toString(), cellDateFormat); //Date
                     Label link = new Label(4, z + 1, Gui.model.getValueAt(z, 4).toString(), hyperlinkCellFormat); //Link
-                    
+
                     WritableHyperlink hyperlink = new WritableHyperlink(4, z + 1, new URL(Gui.model.getValueAt(z, 4).toString()));
-                    
+
                     page.addHyperlink(hyperlink);
                     page.addCell(jxlNumber);
                     page.addCell(source);
@@ -147,7 +121,7 @@ public class ExportToExcel implements ExportManager{
                     page.addCell(link);
                     page.setRowView(z + 1, 600);
                 }
-                
+               
                 newExcel.write();
                 newExcel.close();
                 
@@ -159,8 +133,19 @@ public class ExportToExcel implements ExportManager{
             LOG.warn("export exception");
         }
     }
+    
+    private void initPage(WritableSheet page) throws WriteException, RowsExceededException {
+        page.getSettings().setShowGridLines(true);
+        page.setColumnView(0, 10);
+        page.setColumnView(1, 16);
+        page.setColumnView(2, 100);
+        page.setColumnView(3, 30);
+        page.setColumnView(4, 120);
+        page.setRowView(0, 600);
+    }
 
     private void makeExcelHeaders(WritableSheet page, WritableCellFormat cellHeaderFormat) throws WriteException{
+        String headers[] = getHeaders();
         for (int col = 0; col < headers.length; col++) {
             Label cellName = new Label(col, 0, headers[col], cellHeaderFormat);
             page.addCell(cellName);
