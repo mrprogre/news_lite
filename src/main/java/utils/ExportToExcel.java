c44a66d962ab3d3a1bf787941c77563862a55f7e
hyun0404woo <hyun0404woo@gmail.com>
2022-05-18 09:37:59 +0900

    Refactor: Extract createHeader method and Change variables name
    - Create 'makeExcelHeaders
    - Change the variable name to suit the purpose

--- src/main/java/utils/ExportToExcel.java
+++ src/main/java/utils/ExportToExcel.java
@@ -24,6 +24,7 @@ public class ExportToExcel implements ExportManager{
 
     private final String extension = ".xls";
 
+    private String[] headers = {"Number", "Source", "Title", "Date", "Link"};
     private FileNameExtensionFilter extensionFilter;
     private File baseExcelFile;
     private JFileChooser jFileChooser;
@@ -48,10 +49,7 @@ public class ExportToExcel implements ExportManager{
 
     public void exportResultsToExcel() {
         try {
-//            JFileChooser jFileChooser = setJFileChooser();
-
             if (jFileChooser.showDialog(null,"Save") == APPROVE_OPTION) {
-//                File file = new File(jFileChooser.getSelectedFile() + ".xls");
 
                 WritableWorkbook newExcel = Workbook.createWorkbook(baseExcelFile);
                 WritableSheet page = newExcel.createSheet("001", 0);
@@ -74,7 +72,7 @@ public class ExportToExcel implements ExportManager{
                         WritableFont.BOLD, false, UnderlineStyle.NO_UNDERLINE,
                         jxl.format.Colour.BLACK);
 
-                //Hyperlinks
+                // Hyperlinks Format
                 WritableFont hyperlinkFont = new WritableFont(WritableFont.ARIAL, 11, WritableFont.NO_BOLD);
                 hyperlinkFont.setColour(Colour.DARK_GREEN);
                 WritableCellFormat hyperlinkCellFormat = new WritableCellFormat(hyperlinkFont);
@@ -82,53 +80,56 @@ public class ExportToExcel implements ExportManager{
                 hyperlinkCellFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
                 hyperlinkCellFormat.setWrap(true);
 
-                //WritableCellFormat wcf_no_border = new WritableCellFormat(wf);
+                // Title format
+                WritableCellFormat cellTitleFormat = new WritableCellFormat(writableFontNoBold);
+                cellTitleFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellTitleFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellTitleFormat.setWrap(true);
 
-                WritableCellFormat writableCellFormat = new WritableCellFormat(writableFontNoBold);
-                writableCellFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                writableCellFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                writableCellFormat.setWrap(true);
-
-                WritableCellFormat wcfCentreNoBold = new WritableCellFormat(writableFontNoBold);
-                wcfCentreNoBold.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcfCentreNoBold.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcfCentreNoBold.setAlignment(Alignment.CENTRE);
+                // Number Format
+                WritableCellFormat cellNumberFormat = new WritableCellFormat(writableFontNoBold);
+                cellNumberFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellNumberFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellNumberFormat.setAlignment(Alignment.CENTRE);
+                
+                // Source Format
+                WritableCellFormat cellSourceFormat = new WritableCellFormat(writableFontNoBold);
+                cellSourceFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellSourceFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellSourceFormat.setAlignment(Alignment.CENTRE);
 
                 //no bold
-                WritableCellFormat wcf_centre = new WritableCellFormat(writableFontNoBold);
-                wcf_centre.setAlignment(jxl.format.Alignment.CENTRE);
-                wcf_centre.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_centre.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcf_centre.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                WritableCellFormat cellNoBoldFormat = new WritableCellFormat(writableFontNoBold);
+                cellNoBoldFormat.setAlignment(jxl.format.Alignment.CENTRE);
+                cellNoBoldFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellNoBoldFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellNoBoldFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
 
                 //HEADERS: color, bold
-                WritableCellFormat wcf_centre_bold = new WritableCellFormat(writableFontBold);
-                wcf_centre_bold.setAlignment(jxl.format.Alignment.CENTRE);
-                wcf_centre_bold.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcf_centre_bold.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcf_centre_bold.setBackground(Colour.LIGHT_GREEN);
+                WritableCellFormat cellHeaderFormat = new WritableCellFormat(writableFontBold);
+                cellHeaderFormat.setAlignment(jxl.format.Alignment.CENTRE);
+                cellHeaderFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellHeaderFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellHeaderFormat.setBackground(Colour.LIGHT_GREEN);
 
                 //DATE: no bold
-                DateFormat dateFormat = new DateFormat("dd-MM-yyyy HH:mm") ;
-                WritableCellFormat wcfDate = new WritableCellFormat(dateFormat);
-                wcfDate.setAlignment(jxl.format.Alignment.CENTRE);
-                wcfDate.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcfDate.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
-                wcfDate.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
-                wcfDate.setAlignment(Alignment.CENTRE);
+                DateFormat dataFormat = new DateFormat("dd-MM-yyyy HH:mm");
+                WritableCellFormat cellDateFormat = new WritableCellFormat(dataFormat);
+                
+                cellDateFormat.setAlignment(jxl.format.Alignment.CENTRE);
+                cellDateFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellDateFormat.setBorder(jxl.format.Border.ALL, jxl.format.BorderLineStyle.THIN, jxl.format.Colour.BLACK);
+                cellDateFormat.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+                cellDateFormat.setAlignment(Alignment.CENTRE);
 
-                String[] headers = {"Number", "Source", "Title", "Date", "Link"};
-                for (int col = 0; col < headers.length; col++) {
-                    Label cellName = new Label(col, 0, headers[col], wcf_centre_bold);
-                    page.addCell(cellName);
-                }
+                makeExcelHeaders(page, cellHeaderFormat);
 
                 for (int z = 0; z < Gui.model.getRowCount(); z++) {
-                    jxl.write.Number jxlNumber = new jxl.write.Number(0, z + 1, Integer.parseInt(Gui.model.getValueAt(z, 0).toString()), wcfCentreNoBold); //num
+                    jxl.write.Number jxlNumber = new jxl.write.Number(0, z + 1, Integer.parseInt(Gui.model.getValueAt(z, 0).toString()), cellNumberFormat); //num
                     
-                    Label source = new Label(1, z + 1, Gui.model.getValueAt(z, 1).toString(), wcfCentreNoBold); //Source
-                    Label title = new Label(2, z + 1, Gui.model.getValueAt(z, 2).toString(), writableCellFormat); //Title
-                    Label date = new Label(3, z + 1, Gui.model.getValueAt(z, 3).toString(), wcfDate); //Date
+                    Label source = new Label(1, z + 1, Gui.model.getValueAt(z, 1).toString(), cellSourceFormat); //Source
+                    Label title = new Label(2, z + 1, Gui.model.getValueAt(z, 2).toString(), cellTitleFormat); //Title
+                    Label date = new Label(3, z + 1, Gui.model.getValueAt(z, 3).toString(), cellDateFormat); //Date
                     Label link = new Label(4, z + 1, Gui.model.getValueAt(z, 4).toString(), hyperlinkCellFormat); //Link
                     
                     WritableHyperlink hyperlink = new WritableHyperlink(4, z + 1, new URL(Gui.model.getValueAt(z, 4).toString()));
@@ -151,6 +152,13 @@ public class ExportToExcel implements ExportManager{
         } catch (WriteException | IOException e) {
             Common.console("status: export exception");
             LOG.warn("export exception");
+        }
+    }
+
+    private void makeExcelHeaders(WritableSheet page, WritableCellFormat cellHeaderFormat) throws WriteException{
+        for (int col = 0; col < headers.length; col++) {
+            Label cellName = new Label(col, 0, headers[col], cellHeaderFormat);
+            page.addCell(cellName);
         }
     }
 }
