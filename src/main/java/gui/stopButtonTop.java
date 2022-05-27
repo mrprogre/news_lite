20558829c4f9d318a195339129ab0ebad6543a7e
zer0hoo <62321708+zer0hoo@users.noreply.github.com>
2022-05-27 17:50:02 +0900

    Design Pattern: StopButtonTop(Factory Method), abstract Factory constructor modification

---
+++ src/main/java/gui/stopButtonTop.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class stopButtonTop extends iconButtonFactory {
+    public stopButtonTop(ImageIcon icon, Color color, int x, int y) {
+        super(icon, color, x, y);
+    }
+
+    @Override
+    public void buttonSetting(JButton _btn) {
+        _btn.setIcon(_icon);
+        _btn.setBackground(_color);
+        _btn.setFont(_font);
+        _btn.setBounds(x_axis, y_axis, _width, _height);
+    }
+}
