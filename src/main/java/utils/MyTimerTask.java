99ab2a027a2142ff07363fb3a121f9b0497cb0f3
drok02 <drok02@naver.com>
2022-05-29 23:40:59 +0900

    <Design Pattern> create concreteStrategy SearchMethod

--- src/main/java/utils/MyTimerTask.java
+++ src/main/java/utils/MyTimerTask.java
@@ -2,13 +2,14 @@ package utils;
 
 import gui.Gui;
 import search.Search;
+import search.SearchMethod;
 
 import java.util.TimerTask;
 
 public class MyTimerTask extends TimerTask {
     @Override
     public void run() {
-        Search search = new Search();
+        SearchMethod search = new SearchMethod();
         if (Gui.autoUpdateNewsTop.getState()) {
             search.mainSearch("word");
         } else if (Gui.autoUpdateNewsBottom.getState()) {
