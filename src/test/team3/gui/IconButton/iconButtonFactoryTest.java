c350eff4bf3089774e9916d8d1015226eff4a858
zer0hoo <62321708+zer0hoo@users.noreply.github.com>
2022-06-03 16:26:55 +0900

    Test: add Test cases of main, GUI(iconButton/checkBoxFatory), Utils(Common, MyTimer).

---
+++ src/test/team3/gui/IconButton/iconButtonFactoryTest.java
@@ -0,0 +1,12 @@
+package team3.gui.IconButton;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class iconButtonFactoryTest {
+
+    @Test
+    public void buttonSetting() {
+    }
+}
---
+++ src/test/team3/gui/checkboxFactory/checkBoxFactoryTest.java
@@ -0,0 +1,10 @@
+package team3.gui.checkboxFactory;
+
+import static org.junit.Assert.*;
+
+public class checkBoxFactoryTest {
+
+    @org.junit.Test
+    public void checkBoxSetting() {
+    }
+}
