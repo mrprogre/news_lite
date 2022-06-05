e4eaf8c5bcc301c35fda173dd755611850be270a
arogyaGurkha <kharelarogya@hotmail.com>
2022-06-05 22:17:52 +0900

    Test: Unit test for validating SQLite connection.

--- src/test/team3/database/SQLiteTest.java
+++ src/test/team3/database/SQLiteTest.java
@@ -4,20 +4,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 import static org.junit.Assert.*;
 
 public class SQLiteTest {
+    private SQLite sqLite;
 
     @Before
     public void setUp() throws Exception {
+        sqLite = new SQLite();
     }
 
     @After
     public void tearDown() throws Exception {
+
     }
 
     @Test
-    public void openSQLiteConnection() {
+    public void openSQLiteConnection() throws SQLException {
+        sqLite.openSQLiteConnection();
+        assertTrue(SQLite.connection.isValid(3));
     }
 
     @Test
