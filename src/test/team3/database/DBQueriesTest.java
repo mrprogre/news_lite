646a8d6647749b832e37d623b6d520e8246c757c
arogyaGurkha <kharelarogya@hotmail.com>
2022-06-05 16:38:44 +0900

    Test: Add test files (JUnit4). No test implementation yet.

---
+++ src/test/team3/database/DBQueriesTest.java
@@ -0,0 +1,74 @@
+package team3.database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBQueriesTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void selectSqlite() {
+    }
+
+    @Test
+    public void selectSources() {
+    }
+
+    @Test
+    public void insertNewSource() {
+    }
+
+    @Test
+    public void insertTitleIn256() {
+    }
+
+    @Test
+    public void insertAllTitles() {
+    }
+
+    @Test
+    public void insertNewExcludedWord() {
+    }
+
+    @Test
+    public void deleteTitles() {
+    }
+
+    @Test
+    public void deleteFrom256() {
+    }
+
+    @Test
+    public void deleteSource() {
+    }
+
+    @Test
+    public void deleteDuplicates() {
+    }
+
+    @Test
+    public void updateIsActiveStatus() {
+    }
+
+    @Test
+    public void deleteExcluded() {
+    }
+
+    @Test
+    public void isTitleExists() {
+    }
+
+    @Test
+    public void archiveNewsCount() {
+    }
+}
