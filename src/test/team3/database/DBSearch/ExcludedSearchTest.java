259d92de57099f552b702070793680f93bae97cb
arogyaGurkha <kharelarogya@hotmail.com>
2022-06-05 16:29:24 +0900

    Test: Add test files. No test implementation yet.

---
+++ src/test/team3/database/DBSearch/ExcludedSearchTest.java
@@ -0,0 +1,12 @@
+package team3.database.DBSearch;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExcludedSearchTest {
+
+    @Test
+    void dbSearch() {
+    }
+}
---
+++ src/test/team3/database/SQLiteTest.java
@@ -0,0 +1,20 @@
+package team3.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQLiteTest {
+
+    @Test
+    void openSQLiteConnection() {
+    }
+
+    @Test
+    void closeSQLiteConnection() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+}
