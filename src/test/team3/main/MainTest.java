bf6b0673ccd412e0bec03868e75b8759a6c6ad31
zer0hoo <62321708+zer0hoo@users.noreply.github.com>
2022-06-08 00:01:23 +0900

    Test: Initial file setting test(Main)

--- src/test/team3/main/MainTest.java
+++ src/test/team3/main/MainTest.java
@@ -1,13 +1,39 @@
 package team3.main;
 
 import org.junit.Test;
+import team3.utils.Common;
+
+import java.io.File;
 
 import static org.junit.Assert.*;
 
 public class MainTest {
-
+    /**
+     * Purpose: File directory existing test
+     * input: mainDirectory.exists()
+     * Expected:
+     *  no error with testcase
+     */
     @Test
-    public void main() {
+    public void initialFileSettingTest() {
+        File mainDirectory = new File(Main.DIRECTORY_PATH);
+        assertTrue(mainDirectory.exists());
 
+        File sqliteExeIsExists = new File(Main.DIRECTORY_PATH + "sqlite3.exe");
+        assertTrue(sqliteExeIsExists.exists());
+
+        File sqliteDllIsExists = new File(Main.DIRECTORY_PATH + "sqlite3.dll");
+        assertTrue(sqliteDllIsExists.exists());
+
+        File sqliteDefIsExists = new File(Main.DIRECTORY_PATH + "sqlite3.def");
+        assertTrue(sqliteDefIsExists.exists());
+
+        File dbIsExists = new File(Main.DIRECTORY_PATH + "news.db");
+        assertTrue(dbIsExists.exists());
+
+        File configIsExists = new File(Main.DIRECTORY_PATH + "config.txt");
+        assertTrue(configIsExists.exists());
     }
+
+
 }
