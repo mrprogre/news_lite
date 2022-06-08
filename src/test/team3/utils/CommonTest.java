0244a37cf24ec9b861548667a2aaeba3196a5ed5
zer0hoo <62321708+zer0hoo@users.noreply.github.com>
2022-06-08 17:36:07 +0900

    Test: Config_ToWrite, showDialog, sha256(Common)

--- src/test/team3/utils/CommonTest.java
+++ src/test/team3/utils/CommonTest.java
@@ -1,92 +1,61 @@
 package team3.utils;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import team3.main.Main;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class CommonTest {
-    @Rule
-    public final ErrorCollector collector = new ErrorCollector();
 
-    @Test
-    public void writeToConfig() {
+    /**
+     * Purpose: to cover case branch in getKeywordsFromFile()
+     * input: p_word("hihi") and p_types(for enter each case)
+     * Expected: branch coverage
+     * @throws NullPointerException
+     */
+    @Test(expected = NullPointerException.class)
+    public void Config_ToWriteTest() throws IOException {
+        String p_word = "hihi", p_word2 = "todayOrNotChbx", p_word3 = "filterNewsChbx", p_word4 = "autoSendChbx";
+        String p_type1 = "keyword", p_type2 = "fontColorRed", p_type3 = "fontColorGreen", p_type4 = "fontColorBlue",
+                p_type5 = "backgroundColorRed", p_type6 = "backgroundColorGreen", p_type7 = "backgroundColorBlue",
+                p_type8 = "email", p_type9 = "interval", p_type10 = "checkbox";
+        Common.writeToConfig(p_word, p_type1);
+        Common.writeToConfig(p_word, p_type2);
+        Common.writeToConfig(p_word, p_type3);
+        Common.writeToConfig(p_word, p_type4);
+        Common.writeToConfig(p_word, p_type5);
+        Common.writeToConfig(p_word, p_type6);
+        Common.writeToConfig(p_word, p_type7);
+        Common.writeToConfig(p_word, p_type8);
+        Common.writeToConfig(p_word, p_type9);
+        Common.writeToConfig(p_word, p_type10);
+        Common.writeToConfig(p_word2, p_type10);
+        Common.writeToConfig(p_word3, p_type10);
+        Common.writeToConfig(p_word4, p_type10);
+        Common.writeToConfig("s", "t");
     }
 
-//    @Test(expected = IOException.class)
-//    public void getKeywordsFromFile() throws IOException {
-//        //List<String> lines = Files.readAllLines(Paths.get(Main.SETTINGS_PATH));
-//        assertNotNull(Files.readAllLines(Paths.get(Main.SETTINGS_PATH)));
-//        //collector.addError(new Throwable("getKeywords Error"));
-//    }
-
-//    @Test(expected = IOException.class)
-//    public void getExcludeWordsFromFile() throws IOException {
-//        List<String> lines = Files.readAllLines(Paths.get(Main.SETTINGS_PATH));
-//        collector.addError(new Throwable("getExcludeWords Error"));
-//    }
-//
-//    @Test(expected = IOException.class)
-//    public void getSettingsFromFile() throws IOException {
-//        List<String> lines = Files.readAllLines(Paths.get(Main.SETTINGS_PATH));
-//        collector.addError(new Throwable("getSettings Error"));
-//    }
-//
-//    @Test(expected = IOException.class)
-//    public void getColorsSettingsFromFile() throws IOException {
-//        List<String> lines = Files.readAllLines(Paths.get(Main.SETTINGS_PATH));
-//        collector.addError(new Throwable("getColorSettings Error"));
-//    }
-
-    @Test
-    public void saveState() {
-    }
-
-    @Test
-    public void getSmtp() {
-    }
-
-    @Test
-    public void getEmailSettingsFromFile() {
-    }
-
-    @Test
-    public void countLines() {
-    }
-
-    @Test
-    public void delSettings() {
-    }
-
-    @Test
-    public void console() {
-    }
-
-    @Test
-    public void fill() {
-    }
-
-    @Test
-    public void getInterval() {
-    }
-
-    @Test
-    public void compareDatesOnly() {
-    }
-
+    /**
+     * Purpose: to see coverage
+     * input: showDialog("smi", "log", "excl")
+     * Expected:
+     *  high line coverage
+     */
     @Test
     public void showDialog() {
-    }
-
-    @Test
-    public void copyFiles() {
+        String p_file1 = "smi", p_file2 = "log", p_file3 = "excl";
+        Common.showDialog(p_file1);
+        Common.showDialog(p_file2);
+        Common.showDialog(p_file3);
     }
 
     /**
@@ -103,11 +72,15 @@ public class CommonTest {
         assertEquals("abcdef", sb);
     }
 
+    /**
+     * Purpose: to see coverage
+     * input: sha256("base")
+     * Expected:
+     *  high line coverage
+     */
     @Test
     public void sha256() {
-    }
-
-    @Test
-    public void trayMessage() {
+        String base = "base";
+        Common.sha256(base);
     }
 }
